9c7ac6110e0719bcb1c1a98e8dbe0099e0d5b202
Ryan Compton <ryan.compton@student.sl.on.ca>
2025-12-08 11:27:24 -0500

    finished user interface

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject startMainMenu;
+    public GameObject levelSelect;
+    public void StartGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1;
+    }
+
+    public void GoToLevelSelect()
+    {
+        startMainMenu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
