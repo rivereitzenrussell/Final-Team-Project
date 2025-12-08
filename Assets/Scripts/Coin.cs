990e557d898aa476ce96b769bfe5d710e1c80f63
Kallum McKee <114624315+KallumMcKee@users.noreply.github.com>
2025-12-08 18:48:27 -0500

    Coin UI

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using TMPro;
+using System.Reflection;
 
 public class Coin : MonoBehaviour
 {
+    public AudioClip coinClip;
+    private TextMeshProUGUI coinText;
+
+    private void Start()
+    {
+       coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -9,7 +18,7 @@ public class Coin : MonoBehaviour
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
             player.coins += 1;
             Destroy(gameObject);
-
+            coinText.text = player.coins.ToString();
             SoundManager.Instance.PlaySFX("COIN", 0.4f);
         }
     }
