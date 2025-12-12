8f37582160683752bd2a5d5c38c4c3ecfc3e5753
Liao Chen <iamliaochen@gmail.com>
2025-12-11 22:02:46 -0500

    Level Select & Save Systems

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -5,9 +5,12 @@ public class NextLevel : MonoBehaviour
    
     public string nextLevelName;
 
-   
+    public int nextLevelValue;
+
+
     public void LoadNextLevel()
     {
+        PlayerPrefs.SetInt("LevelReached", nextLevelValue);
 
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevelName);
 
